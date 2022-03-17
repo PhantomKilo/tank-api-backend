a38215fd4fb1f14602a35e53ee7fec4836d3312a
kyle <phantom-kilo@live.com>
2022-03-16 18:03:44 -0700

    Added entered service and description to each afc type

--- models/APC/APC.js
+++ models/APC/APC.js
@@ -1,24 +1,32 @@
-const mongoose = require('../../db/connection'); 
+const mongoose = require("../../db/connection");
 
-const weaponSchema = require('./Weapons');
-const imageSchema = require('./Images')
-    
+const weaponSchema = require("./Weapons");
+const imageSchema = require("./Images");
+
 const apcSchema = new mongoose.Schema({
-    model: {
-      type: String,
-      required: true
-    },
-    country: {
-        type: String,
-        required: true
-    },
-    capacity: {
-        type: Number,
-        required: true
-    },
-    secondary: [weaponSchema],
-    images: [imageSchema]
-  });
-  
-  const APC = mongoose.model('APC', apcSchema);
-  module.exports = APC;
+  model: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  enteredService: {
+    type: Number,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  capacity: {
+    type: Number,
+    required: true,
+  },
+  secondary: [weaponSchema],
+  images: [imageSchema],
+});
+
+const APC = mongoose.model("APC", apcSchema);
+module.exports = APC;
--- models/Tank/Tank.js
+++ models/Tank/Tank.js
@@ -1,26 +1,34 @@
-const mongoose = require('../../db/connection')
+const mongoose = require("../../db/connection");
 
-const weaponSchema = require('./Weapons');
-const ammoSchema = require('./Ammo');
-const imageSchema = require('./Images')
-    
+const weaponSchema = require("./Weapons");
+const ammoSchema = require("./Ammo");
+const imageSchema = require("./Images");
+
 const tankSchema = new mongoose.Schema({
-    model: {
-      type: String,
-      required: true
-    },
-    main: {
-        type: String,
-        required: true
-    },
-    country: {
-        type: String,
-        required: true
-    },
-    ammo: [ammoSchema], 
-    secondary: [weaponSchema],
-    images: [imageSchema]
-  });
-  
-  const Tank = mongoose.model('Tank', tankSchema);
-  module.exports = Tank;
+  model: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  enteredService: {
+    type: Number,
+    required: true,
+  },
+  main: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  ammo: [ammoSchema],
+  secondary: [weaponSchema],
+  images: [imageSchema],
+});
+
+const Tank = mongoose.model("Tank", tankSchema);
+module.exports = Tank;
